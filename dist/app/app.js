4db5fc8f6787e856e605b3ec5c7717dfa57bda52
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-06 01:08:53 +0600

    express js perser and run server

--- dist/app/app.js
+++ dist/app/app.js
@@ -5,7 +5,17 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const app = (0, express_1.default)();
+// parsers json file
+app.use(express_1.default.json());
+// parsers text file
+app.use(express_1.default.text());
 app.get('/', (req, res) => {
-    res.send('hello world');
+    res.send('hello world , I am back !!');
+});
+app.post('/', (req, res) => {
+    console.log(req.body);
+    res.json({
+        message: 'hello from post request'
+    });
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -1,10 +1,23 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 
 
 const app = express();
 
-app.get('/', (req, res) => {
-    res.send('hello world')
+// parsers json file
+app.use(express.json())
+
+// parsers text file
+app.use(express.text())
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('hello world , I am back !!')
+})
+
+app.post('/', (req: Request, res: Response) => {
+    console.log(req.body)
+    res.json({
+        message: 'hello from post request'
+    })
 })
 
 
