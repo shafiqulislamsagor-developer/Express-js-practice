9e97080136ef8fcc9bff893b651463182a2ce345
shafiqulislamsagor-developer <shafiqul.islam.sagor.developer@gmail.com>
2024-10-06 02:00:21 +0600

    type error handler and routes error handler

--- dist/app/app.js
+++ dist/app/app.js
@@ -1,4 +1,13 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
@@ -15,36 +24,94 @@ const courseRouter = express_1.default.Router();
 // use Router
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/course', courseRouter);
-userRouter.get('/create-user', (req, res) => {
-    const user = req.body;
-    console.log(user);
-    res.json({
-        success: true,
-        message: 'user created successfully',
-        data: user
-    });
-});
-courseRouter.get('/create-course', (req, res) => {
-    const course = req.body;
-    res.json({
-        success: true,
-        message: 'course created successfully',
-        data: course
-    });
-});
+userRouter.get('/create-user', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const user = req.body;
+        console.log(user);
+        res.json({
+            success: true,
+            message: 'user created successfully',
+            data: user
+        });
+    }
+    catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     success: false,
+        //     message: 'Error creating user',
+        //     data: null
+        // })
+        next(err);
+    }
+}));
+courseRouter.get('/create-course', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        const course = req.body;
+        res.json({
+            success: true,
+            message: 'course created successfully',
+            data: course
+        });
+    }
+    catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     success: false,
+        //     message: 'Error creating course',
+        //     data: null
+        // })
+        next(err);
+    }
+}));
 // middlewares
 const logger = (req, res, next) => {
     // console.log(req)
     next();
 };
-app.get('/:userId/:name/:year', logger, (req, res) => {
+app.get('/', logger, (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
     // console.log(req.query)
-    res.send('hello world , I am back !!');
-});
-app.post('/', (req, res) => {
-    console.log(req.body);
-    res.json({
-        message: 'hello from post request'
+    try {
+        res.send('hello world , I am back !!');
+    }
+    catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     success: false,
+        //     message: 'Error',
+        //     data: null
+        // })
+        next(err);
+    }
+}));
+app.post('/', (req, res, next) => __awaiter(void 0, void 0, void 0, function* () {
+    try {
+        console.log(req.body);
+        res.json({
+            message: 'hello from post request'
+        });
+    }
+    catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     message: 'Error from post request'
+        // })
+        next(err);
+    }
+}));
+// Routes error handling
+app.all("*", (req, res) => {
+    res.status(404).json({
+        success: false,
+        message: 'Page not found'
     });
+});
+// global error handler
+app.use((error, req, res, next) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message || 'Something went wrong',
+        });
+    }
 });
 exports.default = app;
--- src/app/app.ts
+++ src/app/app.ts
@@ -17,23 +17,43 @@ const courseRouter = express.Router();
 app.use('/api/v1/users', userRouter)
 app.use('/api/v1/course', courseRouter)
 
-userRouter.get('/create-user', (req: Request, res: Response) => {
-    const user = req.body;
-    console.log(user);
-    res.json({
-        success: true,
-        message: 'user created successfully',
-        data: user
-    })
+userRouter.get('/create-user', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const user = req.body;
+        console.log(user);
+        res.json({
+            success: true,
+            message: 'user created successfully',
+            data: user
+        })
+    } catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     success: false,
+        //     message: 'Error creating user',
+        //     data: null
+        // })
+        next(err);
+    }
 })
 
-courseRouter.get('/create-course', (req: Request, res: Response) => {
-    const course = req.body;
-    res.json({
-        success: true,
-        message: 'course created successfully',
-        data: course
-    })
+courseRouter.get('/create-course', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        const course = req.body;
+        res.json({
+            success: true,
+            message: 'course created successfully',
+            data: course
+        })
+    } catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     success: false,
+        //     message: 'Error creating course',
+        //     data: null
+        // })
+        next(err);
+    }
 })
 
 // middlewares
@@ -42,16 +62,52 @@ const logger = (req: Request, res: Response, next: NextFunction) => {
     next()
 }
 
-app.get('/:userId/:name/:year', logger, (req: Request, res: Response) => {
+app.get('/', logger, async (req: Request, res: Response, next: NextFunction) => {
     // console.log(req.query)
-    res.send('hello world , I am back !!')
+    try {
+        res.send('hello world , I am back !!')
+    } catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     success: false,
+        //     message: 'Error',
+        //     data: null
+        // })
+        next(err);
+    }
 })
 
-app.post('/', (req: Request, res: Response) => {
-    console.log(req.body)
-    res.json({
-        message: 'hello from post request'
+app.post('/', async (req: Request, res: Response, next: NextFunction) => {
+    try {
+        console.log(req.body)
+        res.json({
+            message: 'hello from post request'
+        })
+    } catch (err) {
+        // console.error(err);
+        // res.status(500).json({
+        //     message: 'Error from post request'
+        // })
+        next(err);
+    }
+})
+
+// Routes error handling
+app.all("*", (req: Request, res: Response) => {
+    res.status(404).json({
+        success: false,
+        message: 'Page not found'
     })
+})
+
+// global error handler
+app.use((error: any, req: Request, res: Response, next: NextFunction) => {
+    if (error) {
+        res.status(400).json({
+            success: false,
+            message: error.message || 'Something went wrong',
+        })
+    }
 })
 
 
